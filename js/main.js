77ff2d7357548378b0f92001e33d0e01b16940f8
samuel.roussel (karabulut) <zbabug@hotmail.com>
2025-09-26 13:20:35 +0200

    update members

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ window.addEventListener("builderready",async ()=>{
 		App.menu.load(data);
 		routes.force().run();
 		console.log(`photos: %c ${Member.all.filter(o=>o.image).length}/${Member.all.length} (${Member.all.filter(o=>o.image).length*100/Member.all.length|0}%) `,`background-color:#ccc;color:#777`)
+		import_S21();
 	}).catch();
 });
 
@@ -28,3 +29,41 @@ routes.on(()=>{
 	new Fields().clearChanges();
 	App.components.article.auto();
 });
+
+function Log(msg){
+	console.log(msg);
+}
+
+async function import_S21(){
+	let XLSX = Z.XLSX.create();
+	await XLSX.loadData(await builder.get("data/S21.xlsx").base64());
+	let sheet = await XLSX.readSheet(11);
+	console.log(XLSX);
+	console.log(sheet);
+	let rows = sheet.rows;
+	let members = Member.all;
+	let mn = members.map(m=>Z.Tools.normalize(m.fullname).split` `.filter(o=>o));
+	console.log(mn);
+	let m = [];
+	let find_member=name=>{
+		name = {
+			"BITANGA Mélanie-Ernestine":"BITANGA Mélanie",
+			"KALLA EBOUMBOU Marie":"KALLA Marie",
+			"ORLOC COL Marie-Hélène":"ORLOC Marie-Hélène",
+			"SALAGNARD Anny":"SALAGNARD Annie"
+		}[name] || name;
+		let n = Z.Tools.normalize(name).split` `.filter(o=>o);
+		let index = mn.findIndex(a=>a.length==n.length&&!a.some(s=>!n.includes(s)));
+		if (index>=0) return members[index];
+	};
+	for (let i=1;i<rows.length;i++){
+		let row = rows[i];
+		let cell = row.cells.find(o=>o.c==1);
+		if (cell?.v){
+			let member = find_member(cell.v);
+			m.push({name:cell.v,member,row});
+		}
+	}
+	console.log(m);
+	console.log(m.filter(o=>!o.member));
+}
